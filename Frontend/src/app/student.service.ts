23e6e564a5875aae0a4202c82513e89c30799903
divyagaddam786 <divyagaddam786@gmail.com>
2026-06-09 15:56:28 +0530

    update student service and controller

--- Frontend/src/app/student.service.ts
+++ Frontend/src/app/student.service.ts
@@ -5,7 +5,7 @@ import { Student } from './student.model';
 
 @Injectable({ providedIn: 'root' })
 export class StudentService {
-  private url = 'http://localhost:8081/api/students';
+  private url = 'http://3.110.155.2:8081/api/students';
 
   constructor(private http: HttpClient) {}
 
